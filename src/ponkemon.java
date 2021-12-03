48b53bb709d26e0426985c1a4ad46d17a1d171ae
sktom2 <sktom2@naver.com>
2021-12-03 16:43:35 +0900

    폰켓몬

---
+++ src/ponkemon.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+//폰켓몬
+public class ponkemon {
+
+    public int solution(int[] nums) {
+        int answer = 0;
+        int numlength = nums.length/2;
+        HashSet<Integer> num = new HashSet<Integer>();
+        for(int i=0; i<nums.length;i++){
+            num.add(nums[i]);
+        }
+        if(numlength < num.size()) answer =2;
+        else answer = num.size();
+        return answer;
+    }
+    }
+
+
