a863ac4babc21aaf6d8f4923074caf95cbe61f4a
sktom2 <sktom2@naver.com>
2021-12-03 09:49:12 +0900

    음양 더하기

---
+++ src/plus.java
@@ -0,0 +1,12 @@
+// 음양 더하기
+public class plus {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+            for(int i = 0; i <absolutes.length;i++){
+                if(signs[i] == false) absolutes[i] = -1 * absolutes[i];
+                else absolutes[i] = 1 * absolutes[i];
+                answer = answer + absolutes[i];
+            }
+            return answer;
+        }
+}
