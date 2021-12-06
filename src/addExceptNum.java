7edf63ba32c528cb5280dd7e2e0e921eb5ce8c0b
sktom2 <sktom2@naver.com>
2021-12-06 17:25:17 +0900

    없는 숫자 더하기

---
+++ src/addExceptNum.java
@@ -0,0 +1,10 @@
+// 없는 숫자 더하기
+public class addExceptNum {
+  public int solution(int[] numbers) {
+    int answer = 45;
+    for(int i=0; i<numbers.length;i++){
+      answer -= numbers[i];
+    }
+    return answer;
+  }
+}
