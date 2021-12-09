2b40dc242831ef6f222bb17d483b5ffca2905b57
sktom2 <sktom2@naver.com>
2021-12-09 10:41:21 +0900

    나머지가 1이 되는 수 찾기

---
+++ src/remainder1.java
@@ -0,0 +1,13 @@
+public class remainder1 {
+  public static void main(String[] args) {
+    int n = 10;
+    int answer = 0;
+    for (int i = 2; i < n; i++) {
+      if (n % i == 1) {
+        answer = i;
+        break;
+      }
+    }
+      System.out.println(answer);
+    }
+}
