fea76983748952fb92abee6c1fc47b07dafdd73f
JungHyeonwoo <sktom2@naver.com>
2022-01-27 16:11:06 +0900

    2022-01-27

---
+++ src/Programmers/fail/suprised.java
@@ -0,0 +1,15 @@
+package Programmers.fail;
+
+import java.util.Scanner;
+
+public class suprised {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String a = scanner.nextLine();
+
+    String result = a + "??!";
+    System.out.println(result);
+  }
+
+}
