088ab72ccda8bbb134967c7790ed502dd979360b
JungHyeonwoo <sktom2@naver.com>
2022-02-03 10:14:33 +0900

    2022-02-03

---
+++ src/Programmers/Complete/sugi_year.java
@@ -0,0 +1,17 @@
+package Programmers.Complete;
+
+import java.util.Scanner;
+
+public class sugi_year {
+
+
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int a = scanner.nextInt();
+
+      int result = a - 543;
+      System.out.println(result);
+    }
+
+
+}
