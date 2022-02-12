bcce5686a1edaf9e4683b75cd18e83ad25d5d265
JungHyeonwoo <sktom2@naver.com>
2022-02-12 10:30:33 +0900

    [ADD]2022-02-12

---
+++ src/baekjoon/yonsei.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class yonsei {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    if(a ==0){
+      System.out.println("YONSEI");
+    }
+    if(a==1){
+      System.out.println("Leading the Way to the Future");
+    }
+  }
+}
