53603e3bd22f7b46d45c61570118791083b4490a
JungHyeonwoo <sktom2@naver.com>
2022-02-14 15:56:28 +0900

    2022-02-14

---
+++ src/baekjoon/palindrome.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class palindrome {
+
+  public static void main(String[] args) throws Exception{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    while(true){
+      String a = br.readLine();
+      StringBuilder sb = new StringBuilder();
+      sb.append(a);
+      StringBuilder rsb = new StringBuilder();;
+      String d = rsb.append(a).reverse().toString();
+      if(a.equals(String.valueOf(0))) {
+        break;
+      }
+      if(a.equals(d)){
+        System.out.println("yes");
+      }
+      else{
+        System.out.println("no");
+      }
+    }
+  }
+
+}
