6d6f407a2e81d9a0e29b1e653bfdeb7c8548ef52
JungHyeonwoo <sktom2@naver.com>
2022-02-16 14:14:40 +0900

    [ADD]2022-02-16

---
+++ src/baekjoon/Josephus.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+public class Josephus {
+
+  public static void main(String[] args) {;
+
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 1; i <= n; i++) {
+      queue.add(i);
+    }
+    StringBuilder sb = new StringBuilder();
+    sb.append('<');
+
+    while(queue.size() > 1){
+      for(int i=0; i< k-1; i++){
+        int a = queue.poll();
+        queue.add(a);
+      }
+      sb.append(queue.poll()).append(", ");
+    }
+    sb.append(queue.poll()).append('>');
+    System.out.println(sb);
+  }
+}
