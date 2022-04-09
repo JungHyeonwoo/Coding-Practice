92191dddd05e6fc37f630021ce16a8201624cfe3
JungHyeonwoo <sktom2@naver.com>
2022-04-09 16:21:08 +0900

    [ADD] 2022-04-09

---
+++ src/baekjoon/stock.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class stock {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N =  sc.nextInt();
+  }
+
+}
