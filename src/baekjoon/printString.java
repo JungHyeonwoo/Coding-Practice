0ccc55528d8d3598954579387decc1499f6a5a41
JungHyeonwoo <sktom2@naver.com>
2022-06-20 20:27:08 +0900

    [ADD] 2022-06-20

---
+++ src/baekjoon/printString.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.util.*;
+public class printString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+    }
+}
