6094f8b3c1981c6709c857c36485d9fbde13879c
JungHyeonwoo <sktom2@naver.com>
2022-07-03 22:03:52 +0900

    [ADD]2022-07-03

---
+++ src/baekjoon/R2.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class R2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = b*2;
+        System.out.println(sum-a);
+    }
+}
